66e221e3a915300e790ab570019f3b773f7d2f1e
piirk <baptiste.boucharechas@gmail.com>
2025-01-26 17:29:23 +0100

    refactor HomePage to use Ant Design Typography for headings

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react'
 import { states } from '@constants/states'
 import AppLayout from '@common/components/AppLayout'
-import { App } from 'antd'
+import { Typography } from 'antd'
+
+const { Title } = Typography
 
 const HomePage = () => {
   useEffect(() => {
@@ -67,7 +69,7 @@ const HomePage = () => {
   return (
     <AppLayout>
       <a href="employee-list.html">View Current Employees</a>
-      <h2>Create Employee</h2>
+      <Title level={2}>Create Employee</Title>
       <form action="#" id="create-employee">
         <label htmlFor="first-name">First Name</label>
         <input type="text" id="first-name" />
