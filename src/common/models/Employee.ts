80bd6a56f67b29d12e93167cd751d3ab0e3367e8
piirk <baptiste.boucharechas@gmail.com>
2025-01-26 17:35:10 +0100

    add Employee model interface with relevant fields

---
+++ src/common/models/Employee.ts
@@ -0,0 +1,12 @@
+export interface Employee {
+  id: number
+  firstName: string
+  lastName: string
+  dateOfBirth: Date
+  startDate: Date
+  department: string
+  street: string
+  city: string
+  state: string
+  zipCode: string
+}
