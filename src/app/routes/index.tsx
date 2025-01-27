f108b86f4ba6107d736ebaaab82877c18ca5793a
piirk <baptiste.boucharechas@gmail.com>
2025-01-27 17:56:15 +0100

    update routing structure: redirect root to employee creation page and adjust paths for employee pages

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import EmployeeCreationPage from '@features/employeeCreation/EmployeeCreationPage'
 import Error404 from './Error404'
 import EmployeeListPage from '@features/employeeList/EmployeeListPage'
@@ -6,9 +6,9 @@ import EmployeeListPage from '@features/employeeList/EmployeeListPage'
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<EmployeeCreationPage />} />
-      <Route path="/create" element={<EmployeeCreationPage />} />
-      <Route path="/list" element={<EmployeeListPage />} />
+      <Route path="/" element={<Navigate to="/employee/create" />} />
+      <Route path="/employee/create" element={<EmployeeCreationPage />} />
+      <Route path="/employee/list" element={<EmployeeListPage />} />
       <Route path="*" element={<Error404 />} />
     </Routes>
   )
