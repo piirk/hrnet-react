1f8a64e01ea5938c9a282eee35b882ca9b233a1c
piirk <baptiste.boucharechas@gmail.com>
2025-02-12 14:27:32 +0100

    add CssBaseline component for consistent styling and remove unused index.scss

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@redux/store'
 import Routes from './routes'
+import { CssBaseline } from '@mui/material'
 import './App.scss'
 
 const App = () => {
   const dispatch: AppDispatch = useDispatch()
 
-  return <Routes />
+  return (
+    <>
+      <CssBaseline />
+      <Routes />
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from '@redux/store'
-import './index.scss'
-import 'antd/dist/reset.css'
 import App from './app/App'
 
 const rootElement = document.getElementById('root')
