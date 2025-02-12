e39797eb04a63a22ff463a6b23708ca458c4a5d3
piirk <baptiste.boucharechas@gmail.com>
2025-02-12 09:33:31 +0100

    replace mock employee data with API call to fetch employees

--- src/app/redux/actions/employeeActions.ts
+++ src/app/redux/actions/employeeActions.ts
@@ -4,33 +4,8 @@ import { Employee } from '@common/models/Employee'
 export const fetchEmployees = createAsyncThunk(
   'employee/fetchEmployees',
   async () => {
-    // mock data
-    const data: Employee[] = [
-      {
-        id: 1,
-        firstName: 'John',
-        lastName: 'Doe',
-        dateOfBirth: new Date('01-01-1990').toISOString(),
-        startDate: new Date('01-01-2020').toISOString(),
-        department: 'Engineering',
-        street: '123 Main St',
-        city: 'Springfield',
-        state: 'IL',
-        zipCode: '62701',
-      },
-      {
-        id: 2,
-        firstName: 'Jane',
-        lastName: 'Smith',
-        dateOfBirth: new Date('01-01-1990').toISOString(),
-        startDate: new Date('01-01-2020').toISOString(),
-        department: 'Human Resources',
-        street: '123 Main St',
-        city: 'Springfield',
-        state: 'IL',
-        zipCode: '62701',
-      },
-    ]
-    return data
+    const response = await fetch('http://localhost:5000/employees')
+    const data = await response.json()
+    return data as Employee[]
   },
 )
