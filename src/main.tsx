b92cafcd09a10db0812dc7cae9f753368d16a537
piirk <baptiste.boucharechas@gmail.com>
2025-02-12 17:18:45 +0100

    refactor: wrap application in LocalizationProvider for date handling with dayjs

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from '@redux/store'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import App from './app/App'
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
   createRoot(rootElement).render(
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Router>
+          <App />
+        </Router>
+      </LocalizationProvider>
     </Provider>,
   )
 } else {
