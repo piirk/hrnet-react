808c3c36055433920484e0c2960eab53f3a2a120
piirk <baptiste.boucharechas@gmail.com>
2025-02-19 13:13:13 +0100

    refactor: remove unused App.scss styles and import from App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@redux/store'
 import Routes from '../routes'
 import { CssBaseline } from '@mui/material'
-import './App.scss'
 
 const App = () => {
   const dispatch: AppDispatch = useDispatch()
